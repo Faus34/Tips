50c47f6d0abd7c50533931b2d53f19cef7c25c9f
faus34 <faustinogm1997@gmail.com>
2022-03-08 16:26:54 -0600

    basic style added

--- tipCalculator.js
+++ tipCalculator.js
@@ -1,1 +1,13 @@
-console.log('Im the tip calculator js');
+
+let propina = document.getElementById("scale");
+let partes_iguales =  document.getElementById("_checkbox");
+let todos_pagan = false;
+
+function tipChange() {	  //función para el slider de propina  
+ console.log(propina.value);
+}
+
+function partsChange() { //Función para el checkbox
+ todos_pagan = !todos_pagan;
+}
+
